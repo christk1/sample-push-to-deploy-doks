da4cdc0fcb6c0fd945dccc387a409515ae7c4159
christk <chrismousmoulas@outlook.com>
2022-01-08 21:13:28 +0200

    test

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import http.server
 import socketserver
 
 from http import HTTPStatus
-
+# test
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
